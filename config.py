239903995dc96a7ec241e21240d1314ffd5737e1
KJJisBetter <kevinelfuelte@gmail.com>
2024-06-11 19:12:54 +0000

    prod working

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '7q)+870p=)21%ew(3og)+l$4hieg)($a8i5su-&zx&)2f=*9(3'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # print('SQLALCHEMY_DATABASE_URI:', SQLALCHEMY_DATABASE_URI)
